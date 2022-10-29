6bf22be27c7a67d8b3bd8c6be3155b90b45cbcfc
KarimSaleh <eng.karim2010@gmail.com>
2022-10-29 22:59:19 +0400

    battery-45

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -166,7 +166,7 @@ abstract class BaseRepository
         $model = $this->model->newInstance($input);
 
         // dd($this->model);
-        if (! $this->model instanceof \App\Models\User){
+        if (! $this->model instanceof \App\Models\User || ! $this->model instanceof \App\Models\Permission || ! $this->model instanceof \App\Models\Role){
             $model->added_by= auth()->user()->id;
         }
         $model->save();

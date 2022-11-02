73d60c66f225137de0fe898e398b133e294e092b
KarimSaleh <eng.karim2010@gmail.com>
2022-11-03 01:10:13 +0400

    battery-55

---
+++ database/migrations/2022_11_03_000534_create_new_view_battery_add_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewViewBatteryAddTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("
+
+        CREATE VIEW batteries_week_count_chart
+        AS
+        SELECT WEEK(`install_date`,2) AS week, COUNT(DISTINCT `site__deployed`) AS num_of_Site_per_week
+        FROM `battery_add` GROUP BY Week(install_date,2);
+
+
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropView('batteries_week_count_chart');
+        // Schema::dropView('batteries_progress_chart');
+    }
+}
---
+++ database/migrations/2022_11_03_010241_create_view_battery_week_count_chart_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateViewBatteryWeekCountChartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("
+        CREATE VIEW batteries_progress_chart
+        AS
+        SELECT `week`, SUM(`num_of_Site_per_week`) Over (order by week) AS battery_total_progress
+        FROM `batteries_week_count_chart`;
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('batteries_progress_chart');
+    }
+}

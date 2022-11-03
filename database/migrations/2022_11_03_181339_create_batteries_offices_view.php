6b83d3937ad742a9eadfc645bccf1d2a9b986345
KarimSaleh <eng.karim2010@gmail.com>
2022-11-03 19:03:48 +0400

    battery-59

---
+++ database/migrations/2022_11_03_181339_create_batteries_offices_view.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBatteriesOfficesView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("
+        CREATE VIEW batteries_office_progress
+        AS
+            SELECT offices.name as office_name,COUNT(DISTINCT battery_add.site__deployed) as num_office_name
+            FROM `battery_add`
+            JOIN `sites` on (sites.id=battery_add.site__deployed)
+            RIGHT JOIN `offices` on (offices.id=sites.office_id)
+            group by offices.name;
+
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('batteries_offices_progress');
+    }
+}

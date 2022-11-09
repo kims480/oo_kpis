3be708dbd15aac06141a68d38c281c5b3428a87d
KarimSaleh <eng.karim2010@gmail.com>
2022-11-10 01:21:02 +0400

    battery-93

--- resources/views/battery_adds/fields.blade.php
+++ resources/views/battery_adds/fields.blade.php
@@ -14,7 +14,7 @@
 <div class="form-group col-sm-6">
     {!! Form::label('shelter_num', __('models/batteryAdds.fields.shelter_num') . ':') !!}
     {!! Form::number('shelter_num', null, [
-        'class' =>   $errors->has('shelter_num') ? ' is-invalid':'' ,
+        'class' =>  $errors->any()? ( $errors->has('shelter_num') ? ' is-invalid': 'is-valid'):null,
         'min' => 1,
         'max' => 10,
         'placeholder' => 'Enter Selter number',
@@ -28,7 +28,7 @@
 <div class="form-group col-sm-6">
     {!! Form::label('ref_wo', __('models/batteryAdds.fields.ref_wo') . ':') !!}
     {!! Form::text('ref_wo', old('ref_wo'), [
-        'class' =>  $errors->has('ref_wo') ? ' is-invalid': null,
+        'class' =>  $errors->any()? ($errors->has('ref_wo') ? ' is-invalid': 'is-valid'):null,
         'placeholder' => 'Enter WO number',
     ]) !!}
     @error('ref_wo')
@@ -40,18 +40,16 @@
 <div class="form-group col-sm-6">
     {!! Form::label('ref_cr', __('models/batteryAdds.fields.ref_cr') . ':') !!}
     {!! Form::text('ref_cr', old('ref_cr'), [
-        'class' =>  $errors->has('ref_cr') ? ' is-invalid': null,
+        'class' =>  '',
         'placeholder' => 'Enter CR number',
     ]) !!}
-    @error('ref_cr')
-        <div class="text-red-600 text-xs">{{ $message }}</div>
-    @enderror
+
 </div>
 <!-- Load Vlot Before Cr Start -->
 <div class="form-group col-sm-6">
     {!! Form::label('volt_before', __('models/batteryAdds.fields.volt_before') . ':') !!}
     {!! Form::number('volt_before', old('volt_before'), [
-        'class' =>  $errors->has('volt_before') ? ' is-invalid': null,
+        'class' =>  $errors->any()? ($errors->has('volt_before') ? ' is-invalid': 'is-valid'):null,
         'placeholder' => 'Volt before Activity',
         'min' => 46,
         'max' => 64,
@@ -65,7 +63,7 @@
 <div class="form-group col-sm-6">
     {!! Form::label('Amp_before', __('models/batteryAdds.fields.Amp_before') . ':') !!}
     {!! Form::number('Amp_before', old('Amp_before'), [
-        'class' =>  $errors->has('Amp_before') ? ' is-invalid': null,
+        'class' =>  $errors->any()? ($errors->has('Amp_before') ? ' is-invalid': 'is-valid'):null,
         'placeholder' => 'Ampere Befroe Activity',
         'min' => 1,
         'max' => 200,
@@ -82,7 +80,7 @@
     <div class="form-group col-sm-6">
         {!! Form::label('batter_1_sn', __('models/batteryAdds.fields.batter_1_sn') . ':') !!}<small class="text-red-800 text-base font-bold">*</small>
         {!! Form::text('batter_1_sn', old('batter_1_sn'), [
-            'class' =>  $errors->has('batter_1_sn') ? ' is-invalid': null,
+            'class' =>  $errors->any()? ($errors->has('batter_1_sn') ? ' is-invalid': 'is-valid'):null,
         ]) !!}
         @error('batter_1_sn')
             <div class="text-red-600 text-xs">{{ $message }}</div>
@@ -91,7 +89,7 @@
     <div class="form-group col-sm-6">
         {!! Form::label('battery_2_sn', __('models/batteryAdds.fields.battery_2_sn') . ':') !!}<small class="text-red-800 text-base font-bold">*</small>
         {!! Form::text('battery_2_sn', old('battery_2_sn'), [
-            'class' =>  $errors->has('battery_2_sn') ? ' is-invalid': null,
+            'class' =>  $errors->any()? ($errors->has('battery_2_sn') ? ' is-invalid': 'is-valid'):null,
         ]) !!}
         @error('battery_2_sn')
             <div class="text-red-600 text-xs">{{ $message }}</div>
@@ -100,7 +98,7 @@
     <div class="form-group col-sm-6">
         {!! Form::label('battery_3_sn', __('models/batteryAdds.fields.battery_3_sn') . ':') !!}<small class="text-red-800 text-base font-bold">*</small>
         {!! Form::text('battery_3_sn', old('battery_3_sn'), [
-            'class' =>  $errors->has('battery_3_sn') ? ' is-invalid': null,
+            'class' =>  $errors->any()? ($errors->has('battery_3_sn') ? ' is-invalid': 'is-valid'):null,
         ]) !!}
         @error('battery_3_sn')
             <div class="text-red-600 text-xs">{{ $message }}</div>
@@ -109,7 +107,7 @@
     <div class="form-group col-sm-6">
         {!! Form::label('battery_4_sn', __('models/batteryAdds.fields.battery_4_sn') . ':') !!}<small class="text-red-800 text-base font-bold">*</small>
         {!! Form::text('battery_4_sn', old('battery_4_sn'), [
-            'class' =>  $errors->has('battery_4_sn') ? ' is-invalid': null,
+            'class' =>  $errors->any()? ($errors->has('battery_4_sn') ? ' is-invalid': 'is-valid'):null,
         ]) !!}
         @error('battery_4_sn')
             <div class="text-red-600 text-xs">{{ $message }}</div>
@@ -122,7 +120,7 @@
 <div class="form-group col-sm-6">
     {!! Form::label('num_of_rect', __('models/batteryAdds.fields.num_of_rect') . ':') !!}
     {!! Form::number('num_of_rect', old('num_of_rect'), [
-        'class' =>  $errors->has('num_of_rect') ? ' is-invalid': null,
+        'class' =>  $errors->any()? ($errors->has('num_of_rect') ? ' is-invalid': 'is-valid'):null,
         'min' => 1,
         'max' => 10,
         'placeholder' => 'How many Rectifiers on site?',
@@ -136,7 +134,7 @@
 <div class="form-group col-sm-6">
     {!! Form::label('rect_num', __('models/batteryAdds.fields.rect_num') . ':') !!}
     {!! Form::number('rect_num', old('rect_num'), [
-        'class' =>  $errors->has('rect_num') ? ' is-invalid': null,
+        'class' =>  $errors->any()? ($errors->has('rect_num') ? ' is-invalid': 'is-valid'):null,
         'min' => 1,
         'max' => 10,
         'placeholder' => 'Which Rectifier Used for this Batteries?',
@@ -150,7 +148,7 @@
 <div class="form-group col-sm-6">
     {!! Form::label('bank_num', __('models/batteryAdds.fields.bank_num') . ':') !!}
     {!! Form::number('bank_num', old('bank_num'), [
-        'class' =>  $errors->has('bank_num') ? ' is-invalid' : null,
+        'class' =>  $errors->any()? ($errors->has('bank_num') ? ' is-invalid': 'is-valid'):null,
         'min' => 1,
         'max' => 20,
         'placeholder' => 'Which Batteru Bank this batteries?',
@@ -206,7 +204,7 @@
     <div class="form-group col-sm-6">
         {!! Form::label('old_batteries_aging', __('models/batteryAdds.fields.old_batteries_aging') . ':') !!}
         {!! Form::number('old_batteries_aging', old('old_batteries_aging'), [
-            'class' =>  $errors->has('old_batteries_aging') ? ' is-invalid': null,
+            'class' =>  $errors->any()? ($errors->has('old_batteries_aging') ? ' is-invalid': 'is-valid'):null,
             'min' => 0,
             'max' => 10,
             'placeholder' => 'Old Batteries Aging',
@@ -218,7 +216,7 @@
     <div class="form-group col-sm-6">
         {!! Form::label('llvd', __('models/batteryAdds.fields.llvd') . ':') !!}
         {!! Form::number('llvd', old('llvd'), [
-            'class' =>  $errors->has('llvd') ? ' is-invalid': null,
+            'class' =>  $errors->any()? ($errors->has('llvd') ? ' is-invalid': 'is-valid'):null,
             'placeholder' => 'LLVD Value in rectifier (46v)',
             'min' => 46,
             'max' => 64,
@@ -233,7 +231,7 @@
     <div class="form-group col-sm-6">
         {!! Form::label('blvd', __('models/batteryAdds.fields.blvd') . ':') !!}
         {!! Form::number('blvd', old('blvd'), [
-            'class' =>  $errors->has('blvd') ? ' is-invalid': null,
+            'class' =>  $errors->any()? ($errors->has('blvd') ? ' is-invalid': 'is-valid'):null,
             'placeholder' => 'BLVD Value in rectifier (43.2v)',
             'min' => 42,
             'max' => 48,
@@ -247,7 +245,7 @@
     <div class="form-group col-sm-6">
         {!! Form::label('capacity_rating', __('models/batteryAdds.fields.capacity_rating') . ':') !!}
         {!! Form::number('capacity_rating', old('capacity_rating'), [
-            'class' =>  $errors->has('capacity_rating') ? ' is-invalid': null,
+            'class' =>  $errors->any()? ($errors->has('capacity_rating') ? ' is-invalid': 'is-valid'):null,
             'placeholder' => 'Capacity Ratin Should match batteries rating (Ah)',
             'min' => 90,
             'max' => 400,
@@ -261,7 +259,7 @@
     <div class="form-group col-sm-6">
         {!! Form::label('battery_brand', __('models/batteryAdds.fields.battery_brand') . ':') !!}
         {!! Form::text('battery_brand', 'SACRED SUN', [
-            'class' =>  $errors->has('battery_brand') ? ' is-invalid': null,
+            'class' =>  $errors->any()? ($errors->has('battery_brand') ? ' is-invalid': 'is-valid'):null,
             'placeholder' => 'Batteries manufacturer',
         ]) !!}
         @error('battery_brand')
@@ -271,7 +269,9 @@
     <!-- Battery Model Field -->
     <div class="form-group col-sm-6">
         {!! Form::label('Battery_model', __('models/batteryAdds.fields.Battery_model') . ':') !!}
-        {!! Form::text('Battery_model', 'FTA12-190A', [$errors->has('Battery_model') ? ' is-invalid': null, 'placeholder' => 'Batteries Model']) !!}
+        {!! Form::text('Battery_model', 'FTA12-190A', [
+            $errors->any()? ($errors->has('Battery_model') ? ' is-invalid': 'is-valid'):null,
+             'placeholder' => 'Batteries Model']) !!}
         @error('Battery_model')
             <div class="text-red-600 text-xs">{{ $message }}</div>
         @enderror
@@ -280,7 +280,7 @@
     <div class="form-group col-sm-6">
         {!! Form::label('Volt_after', __('models/batteryAdds.fields.Volt_after') . ':') !!}
         {!! Form::number('Volt_after', old('Volt_after'), [
-            'class' =>  $errors->has('Volt_after') ? ' is-invalid': null,
+            'class' =>  $errors->any()? ($errors->has('Volt_after') ? ' is-invalid': 'is-valid'):null,
             'placeholder' => 'Volt After Replacing Batteries',
             'min' => 46,
             'max' => 64,
@@ -294,7 +294,7 @@
     <div class="form-group col-sm-6">
         {!! Form::label('Amp_After', __('models/batteryAdds.fields.Amp_After') . ':') !!}
         {!! Form::number('Amp_After', old('Amp_After'), [
-            'class' =>  $errors->has('Amp_After') ? ' is-invalid': null,
+            'class' =>  $errors->any()? ($errors->has('Amp_After') ? ' is-invalid': 'is-valid'):null,
             'placeholder' => 'Ampere After Replacing Batteries',
             'min' => 1,
             'max' => 200,
@@ -308,7 +308,7 @@
     <div class="form-group col-sm-6">
         {!! Form::label('remarks', __('models/batteryAdds.fields.remarks') . ':') !!}
         {!! Form::text('remarks', old('remarks'), [
-            'class' =>  $errors->has('remarks') ? ' is-invalid': null,
+            'class' =>  $errors->any()? ($errors->has('remarks') ? ' is-invalid': 'is-valid'):null,
             'placeholder' => 'Please add if any remarks',
         ]) !!}
         @error('remarks')

856be0a4aa8823f90b212e9c4f16cf6af4a07eaa
KarimSaleh <eng.karim2010@gmail.com>
2023-06-24 07:11:05 +0400

    update consumable_4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -154,43 +154,7 @@
 
                 <!-- Dropdown Nav -->
                 <nav :class="isOpen ? 'flex': 'hidden'" class="flex flex-col pt-4 bg-slate-800">
-                    <a href="{{route('dashboard')}}"
-                        class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-tachometer-alt mr-3"></i>
-                        Dashboard
-                    </a>
-                    <a href="{{ route('site-snags.index') }}"
-                        class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-sticky-note mr-3"></i>
-                        Snags
-                    </a>
-                    <a href="{{ route('site-snags.create') }}"
-                        class="flex items-center active-nav-link text-white py-2 pl-4 nav-item">
-                        <i class="fas fa-table mr-3"></i>
-                        Add Snag
-                    </a>
-
-                    <a href="{{-- {{ route('user.profile') }} --}}"
-                        class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-user mr-3"></i>
-                        My Profile
-                    </a>
-                    <a href="{{ route(__('models/batteryAdds.singular').'.index') }}"
-                        class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-user mr-3"></i>
-                        Deploy Battery
-                    </a>
-                    <a href="{{ route(__('models/consumableSpares.route').'.index') }}"
-                        class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-user mr-3"></i>
-                        Consumable spare
-                    </a>
-                    <a href="#"
-                        class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item"
-                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        <i class="fas fa-sign-out-alt mr-3"></i>
-                        Sign Out
-                    </a>
+                    @include('layouts.menu_mobile')
 
                 <form id="logout-form" action="{{ route('logout') }}" method="POST"
                     class="d-none">

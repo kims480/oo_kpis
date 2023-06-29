10ada9df5233cfaaa1a356556c6e0c1f5f9f5545
KarimSaleh <eng.karim2010@gmail.com>
2023-06-29 04:04:55 +0400

    consumable move_5

--- resources/views/livewire/consumable-form.blade.php
+++ resources/views/livewire/consumable-form.blade.php
@@ -126,7 +126,7 @@
         </div>
 
 
-        <div class="flex justify-end  py-1 bg-slate-50">
+        <div class="flex justify-end  py-1 bg-blue-50">
             <input
                 class="hover:bg-green-800 cursor-pointer py-1 px-3 bg-green-600 border border-indigo-600 rounded-md text-white focus:outline-none"
                 type="submit" value="Submit">

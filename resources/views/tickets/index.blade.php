81a822e1e0aecfc7bc0c5d05dec5fd4c69ad51c9
KarimSaleh <eng.karim2010@gmail.com>
2023-07-14 18:22:17 +0400

    table_template_13

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="content-header">
+    {{-- <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
                    @lang('models/tickets.plural')
                 </div>
-                <div class="col-sm-6 d-flex justify-content-end">
+                <div class="flex justify-content-end">
                     <a class="btn btn-primary mr-3"
                        href="{{ route('tickets.create') }}">
                          @lang('crud.add_new')
@@ -22,6 +22,16 @@
                 @include('layouts.app.common')
             </div>
         </div>
+    </section> --}}
+
+    <section class="content-header bg-primary p-0">
+        <h3>@lang('models/tickets.plural')</h3>
+        <a class="btn btn-default btn-sm ml-auto" href="{{ route('tickets.create') }}">
+            @lang('crud.add_new')
+        </a>
+        <a class="btn btn-default btn-sm ml-2" href="{{ route('insert.permissions', __('models/tickets.singular')) }}">
+            @lang('crud.initiate_permissions')
+        </a>
     </section>
 
     <div class="content px-3">

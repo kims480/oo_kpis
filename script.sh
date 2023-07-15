b28a3cc0304be2d0b76867f1b3a946e4806251d7
KarimSaleh <eng.karim2010@gmail.com>
2023-07-15 04:24:05 +0400

    cron_job_update6

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd /home/u459353948/domains/karimsaleh.tech/public_html/ookpi && '/opt/alt/php80/usr/bin/php' artisan schedule:run > /dev/null 2>&1
+'/opt/alt/php80/usr/bin/php' artisan schedule:run > /dev/null 2>&1

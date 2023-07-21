4e0c5572524c331eb5fce9a31d78124e5516a5e0
KarimSaleh <eng.karim2010@gmail.com>
2023-07-21 14:29:06 +0400

    update_tickets_4

--- resources/views/layouts/menu_2.blade.php
+++ resources/views/layouts/menu_2.blade.php
@@ -929,7 +929,7 @@ $urlAdmin = config('fast.admin_prefix');
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                 </svg>
-                <h3 x-cloak class="text-base"
+                <h3 x-cloak class="text-base uppercase"
                     x-bind:class="!$store.sidebar.full && show ? visibleClass : '' || !$store.sidebar.full && !show ? 'sm:hidden' : ''">
                     <p>@lang('models/tickets.route')</p>
                 </h3>
--- resources/views/layouts/menu_mobile.blade.php
+++ resources/views/layouts/menu_mobile.blade.php
@@ -1,37 +1,47 @@
-<a href="{{route('dashboard')}}"
-                        class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-tachometer-alt mr-3"></i>
-                        Dashboard
-                    </a>
-                    <a href="{{ route('site-snags.index') }}"
-                        class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-sticky-note mr-3"></i>
-                        Snags
-                    </a>
-                    <a href="{{ route('site-snags.create') }}"
-                        class="flex items-center active-nav-link text-white py-2 pl-4 nav-item">
-                        <i class="fas fa-table mr-3"></i>
-                        Add Snag
-                    </a>
+<a href="{{ route('dashboard') }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
+    <i class="fas fa-tachometer-alt mr-3"></i>
+    Dashboard
+</a>
+<a href="{{ route('site-snags.index') }}"
+    class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
+    <i class="fas fa-sticky-note mr-3"></i>
+    Snags
+</a>
+@can(__('models/site-snags.singular') . '.index')
 
-                    <a href="{{-- {{ route('user.profile') }} --}}"
-                        class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-user mr-3"></i>
-                        My Profile
-                    </a>
-                    <a href="{{ route(__('models/batteryAdds.singular').'.index') }}"
-                        class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-user mr-3"></i>
-                        Deploy Battery
-                    </a>
-                    <a href="{{ route(__('models/consumableSpares.route').'.index') }}"
-                        class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-user mr-3"></i>
-                        Consumable spare
-                    </a>
-                    <a href="#"
-                        class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item"
-                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        <i class="fas fa-sign-out-alt mr-3"></i>
-                        Sign Out
-                    </a>
+<a href="{{ route('site-snags.create') }}" class="flex items-center active-nav-link text-white py-2 pl-4 nav-item">
+    <i class="fas fa-table mr-3"></i>
+    Add Snag
+</a>
+@endcan
+<a href="{{-- {{ route('user.profile') }} --}}" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
+    <i class="fas fa-user mr-3"></i>
+    My Profile
+</a>
+@can(__('models/batteryAdds.singular') . '.index')
+    <a href="{{ route(__('models/batteryAdds.singular') . '.index') }}"
+        class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
+        <i class="fas fa-user mr-3"></i>
+        Deploy Battery
+    </a>
+@endcan
+@can(__('models/consumableSpares.route') . '.index')
+
+    <a href="{{ route(__('models/consumableSpares.route') . '.index') }}"
+        class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
+        <i class="fas fa-user mr-3"></i>
+        Consumable spare
+    </a>
+@endcan
+@hasanyrole('supper-admin|OTC')
+    <a href="{{ route(__('models/tickets.route') . '.index') }}"
+        class="flex items-center uppercase text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
+        <i class="fas fa-user mr-3"></i>
+        {{ __('models/tickets.route') }}
+    </a>
+@endhasanyrole
+<a href="#" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item"
+    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+    <i class="fas fa-sign-out-alt mr-3"></i>
+    Sign Out
+</a>
